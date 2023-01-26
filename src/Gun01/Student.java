50b16419b9736f497e0c3f06cecaff78d9d5c11d
Alperen Demirci <alperndmrcii@gmail.com>
2023-01-26 19:23:44 +0300

    ilk task yapıldı

---
+++ src/Gun01/Student.java
@@ -0,0 +1,10 @@
+package Gun01;
+
+public class Student {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <3 ; i++) {
+            System.out.println("Hello "+i);
+        }
+    }
+}
